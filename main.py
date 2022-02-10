e79027d4a9b308fa3d37ed3e3f9a6ce7976777e4
Mikail Bakulin <bakulin.mv@gmail.com>
2022-02-10 18:10:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+# читаем адресную книгу в формате CSV в список contacts_list
+import csv
+with open("phonebook_raw.csv") as f:
+  rows = csv.reader(f, delimiter=",")
+  contacts_list = list(rows)
+pprint(contacts_list)
+
+
+
+# TODO 2: сохраните получившиеся данные в другой файл
+# код для записи файла в формате CSV
+with open("phonebook.csv", "w") as f:
+  datawriter = csv.writer(f, delimiter=',')
+  # Вместо contacts_list подставьте свой список
+  datawriter.writerows(contacts_list)
